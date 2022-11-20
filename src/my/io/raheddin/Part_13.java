f4f6e10a021664a1bd191d3777a40882c6d2ba79
Rahaddin Asadzade <rahaddinasad@gmail.com>
2022-11-20 11:14:46 +0400

    FileReader

---
+++ src/my/io/raheddin/Part_13.java
@@ -0,0 +1,25 @@
+package my.io.raheddin;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Part_13 {
+    /*FileReader
+     Java.io paketinin FileReader sınıfı, dosyalardan verileri (karakter olarak) okumak için kullanılabilir. InputStreamReader sınıfını genişletir.
+     */
+    public static void main(String[] args) {
+
+        try {
+            FileReader fileReader=new FileReader("Raxa.txt");
+            int i=fileReader.read();
+            while (i!=-1){
+                System.out.print((char) i);
+                i=fileReader.read();
+            }
+            fileReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
