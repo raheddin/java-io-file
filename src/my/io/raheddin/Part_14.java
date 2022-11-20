9ebac7f1b42897c99791206681ee458330229c98
Rahaddin Asadzade <rahaddinasad@gmail.com>
2022-11-20 11:19:51 +0400

    FileWriter

---
+++ src/my/io/raheddin/Part_14.java
@@ -0,0 +1,21 @@
+package my.io.raheddin;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Part_14 {
+    /*FileWriter
+Java.io paketinin FileWriter sınıfı, dosyalara (karakter olarak) veri yazmak için kullanılabilir. OutputStreamWriter sınıfını genişletir.
+     */
+    public static void main(String[] args) {
+        try {
+            String data="Rahəddin ıəpöğçş";
+            FileWriter fileWriter=new FileWriter("Yeni..txt");
+            fileWriter.write(data);
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
